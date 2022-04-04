723fd6c81ee3892cd2c3ddb48b26bff9dfb6e4f1
N008Coder <100627305+N008Coder@users.noreply.github.com>
2022-04-03 22:09:22 -0700

    Add files via upload
    Moved the save as CSV function into the utilities directory for the sake of cleanliness. Then made sure the correct Imports were being used in each directory. Lastly, to take care of an edge case, I used an If-Else statement to print a different line if 0 loans are found.

--- loan_qualifier_app/app.py
+++ loan_qualifier_app/app.py
@@ -9,10 +9,10 @@ Example:
 import sys
 import fire
 import questionary
-import csv
 from pathlib import Path
 
-from qualifier.utils.fileio import load_csv
+
+from qualifier.utils.fileio import load_csv, save_qualifying_loans
 
 from qualifier.utils.calculators import (
     calculate_monthly_debt_ratio,
@@ -98,34 +98,15 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
     bank_data_filtered = filter_debt_to_income(monthly_debt_ratio, bank_data_filtered)
     bank_data_filtered = filter_loan_to_value(loan_to_value_ratio, bank_data_filtered)
 
-    print(f"Found {len(bank_data_filtered)} qualifying loans")
+    # Small modification for edge cases
+    if len(bank_data_filtered) > 0:
+        print(f"Found {len(bank_data_filtered)} qualifying loans")
+    else:
+        print("Sorry, there are no loans available to you at the moment.")
 
     return bank_data_filtered
 
 
-def save_qualifying_loans(qualifying_loans):
-    """Saves the qualifying loans to a CSV file.
-
-    Args:
-        qualifying_loans (list of lists): The qualifying bank loans.
-    """
-    # @TODO: Complete the usability dialog for savings the CSV Files.
-
-# Set the output header
-    header = ["Lender - Loan Level", "Max Loan Amount", "Max LTV", "Max DTI","Min Credit Score","Interest Rate"]
-
-    output_path = Path("qualifying_loans.csv")
-
-
-    with open(output_path, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(header)
-        for row in qualifying_loans:
-            csvwriter.writerow(row)
-
-
-# Above, we call upon the imported modules to create the set-up and then run the set-up to save the filtered list of dictionaries (inexpensive_loans)
-# and save that list into a CSV file by separating out each value in the dictionary as a comma-delineated value and showing the Keys as a header in the 1st row    # YOUR CODE HERE!
 
 
 def run():
--- loan_qualifier_app/qualifier/utils/fileio.py
+++ loan_qualifier_app/qualifier/utils/fileio.py
@@ -1,10 +1,35 @@
 # -*- coding: utf-8 -*-
-"""Helper functions to load and save CSV data.
+"""Helper functions to load ("load_csv") and save ("save_qualifying_loans") CSV data.
 
 This contains a helper function for loading and saving CSV files.
 
 """
 import csv
+from pathlib import Path
+
+def save_qualifying_loans(qualifying_loans):
+    """Saves the qualifying loans to a CSV file.
+
+    Args:
+        qualifying_loans (list of lists): The qualifying bank loans.
+    """
+    # @TODO: Complete the usability dialog for savings the CSV Files.
+
+# Set the output header
+    header = ["Lender - Loan Level", "Max Loan Amount", "Max LTV", "Max DTI","Min Credit Score","Interest Rate"]
+
+    output_path = Path("qualifying_loans.csv")
+
+
+    with open(output_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(header)
+        for row in qualifying_loans:
+            csvwriter.writerow(row)
+
+
+# Above, we call upon the imported modules to create the set-up and then run the set-up to save the filtered list of dictionaries (inexpensive_loans)
+# and save that list into a CSV file by separating out each value in the dictionary as a comma-delineated value and showing the Keys as a header in the 1st row    # YOUR CODE HERE!
 
 
 def load_csv(csvpath):
